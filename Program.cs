1409f67dcf54e2a00f39fa773bc26c8735265fe0
Armen <aapapikyan@gmail.com>
2023-02-05 14:18:52 +0300

    Решение задачи 1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите трехзначное число");
+String number = Console.ReadLine();
+int a = Convert.ToInt32(number);
+if(a>99 && a<1000)
+{
+    int thirdDigit = a%10;
+    a = a - thirdDigit;
+    a = a/10;
+    a = a%10; 
+    Console.WriteLine($"Вторая цифра {a}");    
+}
+else
+{
+    Console.WriteLine("Вы ввели нетрехзначное Число");
+}
